e810f13d8ebe25518798d449339c09198036f04c
gaazoo <gaazoo@yandex.ru>
2023-07-25 12:01:36 +0700

    Домашка готова

---
+++ task41/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите элементы (через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int count = array.Count(x => x > 0);
+Console.WriteLine($"Количество положительных элементов = {count}");
